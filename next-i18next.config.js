bb37e465afb97557a46947fff6d870692478e87b
amineouabbou <amineouabbou@gmail.com>
2023-04-24 21:45:46 +0100

    Update next config parameters

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,10 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr'],
+    localePath:
+      typeof window === 'undefined'
+        ? require('path').resolve('./public/locales')
+        : '/locales',
   },
+  react: { useSuspense: false },
 }
